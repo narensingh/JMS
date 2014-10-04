61ef9246575280033b4881c09ae4f0172a3d5e79
nsingh <nsingh@paypal.com>
2014-10-04 11:25:38 -0700

    after linear layout and button handler to change color

--- app/src/main/java/com/test/nsingh/jms/MyActivity.java
+++ app/src/main/java/com/test/nsingh/jms/MyActivity.java
@@ -1,18 +1,47 @@
 package com.test.nsingh.jms;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 public class MyActivity extends Activity {
 
+    LinearLayout background;
+    Button btnGreen;
+    Button btnBlue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        //setContentView(R.layout.activity_my);
+        setContentView(R.layout.home_screen);  //home_screen.xml is the layout file
+
+        background  =  (LinearLayout) findViewById(R.id.home_screen_background);
+        btnBlue = (Button) findViewById(R.id.btnBlue);
+        btnGreen = (Button) findViewById(R.id.btnGreen);
+
+        btnGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // click button code here.
+                background.setBackgroundColor(Color.GREEN);
+            }
+        });
+
+        btnBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                background.setBackgroundColor(Color.BLUE);
+            }
+        });
     }
+
 
 
     @Override
